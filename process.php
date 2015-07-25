892c54945eba17ff867b2328cea611b4035ec94a
aksdad <akshay@aksdad.com>
2015-07-26 03:09:40 +0530

    implemented REST API

--- process.php
+++ process.php
@@ -4,17 +4,32 @@ header("Access-Control-Allow-Origin: *");
 
 	$m = new MySQL();
 
-	if(isset($_GET['addusers'])){
-		$id = $_GET['trainid'];
-		$num = $_GET['num'];
-		// echo $num;
-		$m->Update("trainData", array("numBheed" => $num), array("id" => $id));
-	}
+	// if(isset($_GET['addusers'])){
+	// 	$id = $_GET['trainid'];
+	// 	$num = $_GET['num'];
+	// 	// echo $num;
+	// 	$m->Update("trainData", array("numBheed" => $num), array("id" => $id));
+	// }
 
-	if(isset($_GET['update'])){
-		$id = $_GET['trainid'];	
-		$m->Select("trainData", array("numBheed"), array("id" => $id));
-		$num = $m->ArrayResult();
-		echo json_encode(Array("status"=>$num["numBheed"]));
+	// if(isset($_GET['update'])){
+	// 	$id = $_GET['trainid'];	
+	// 	$m->Select("trainData", array("numBheed"), array("id" => $id));
+	// 	$num = $m->ArrayResult();
+	// 	echo json_encode(Array("status"=>$num["numBheed"]));
+	// }
+	if(isset($_GET)){
+		if(!isset($_POST['num'])){
+			$id = $_GET['trainid'];	
+			$m->Select("trainData", array("numBheed"), array("id" => $id));
+			$num = $m->ArrayResult();
+			http_response_code(200);
+			echo json_encode(Array("status"=>$num["numBheed"]));
+		}else{
+			$id = $_GET['trainid'];
+			$num = $_POST['num'];
+			$m->Update("trainData", array("numBheed" => $num), array("id" => $id));
+			http_response_code(201);
+			return;
+		}
 	}
 ?>
