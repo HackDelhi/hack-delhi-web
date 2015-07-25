b6280f27ac017a90c132b5187d0225657c9101db
aksdad <akshay@aksdad.com>
2015-07-25 23:38:14 +0530

    multiple trains

--- process.php
+++ process.php
@@ -5,13 +5,15 @@ header("Access-Control-Allow-Origin: *");
 	$m = new MySQL();
 
 	if(isset($_GET['addusers'])){
+		$id = $_GET['trainid'];
 		$num = $_GET['num'];
 		// echo $num;
-		$m->Update("trainData", array("numBheed" => $num), array("id" => '1'));
+		$m->Update("trainData", array("numBheed" => $num), array("id" => $id));
 	}
 
-	if(isset($_GET['update'])){	
-		$m->Select("trainData", array("numBheed"), array("id" => '1'));
+	if(isset($_GET['update'])){
+		$id = $_GET['trainid'];	
+		$m->Select("trainData", array("numBheed"), array("id" => $id));
 		$num = $m->ArrayResult();
 		echo json_encode(Array("status"=>$num["numBheed"]));
 	}
