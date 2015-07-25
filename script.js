abab5ff94033efc2fa7b0007ac1ddc0201ab0938
Akshat Mittal <itsreallyakshat@gmail.com>
2015-07-25 19:25:09 +0530

    Early Front

---
+++ script.js
@@ -0,0 +1,19 @@
+var t,active_state="dem";
+var getStat = function() {
+  clearInterval(t);
+  $.getJSON("?", function(e){
+    console.log(e);
+    //e
+    animate(e.status);
+    t = setTimeout(getStat, 1000);
+  });
+}
+window.onload = function() {
+  t = setTimeout(getStat, 1000);
+}
+
+var animate = function(st) {
+  $(".ac-" + active_state).fadeOut(600);
+  $(".ac-" + st).fadeIn(600);
+  active_state = st;
+}
