081b11011760229e0d19c5c330b43ad04929304e
aksdad <akshay@aksdad.com>
2015-07-25 22:28:23 +0530

    fix typo

--- process.php
+++ process.php
@@ -1,17 +1,18 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 	require("mysql.class.php");
 
 	$m = new MySQL();
 
-	if(isset($_GET['addUsers'])){
+	if(isset($_GET['addusers'])){
 		$num = $_GET['num'];
-		$m->Update("trainData", array("numBheed" => $num));
+		// echo $num;
+		$m->Update("trainData", array("numBheed" => $num), array("id" => '1'));
 	}
 
-	if(isset($_GET['update'])){
+	if(isset($_GET['update'])){	
 		$m->Select("trainData", array("numBheed"), array("id" => '1'));
 		$num = $m->ArrayResult();
 		echo json_encode(Array("status"=>$num["numBheed"]));
-
 	}
 ?>
